b99acf37018c265d33511a75bebbb518dce0b75b
Eduard <gizbreht.e@yandex.ru>
2020-11-24 15:07:20 +0300

    Закончены мелкие правки на соответствие макету, убраны обнаруженные баги

--- src/blocks/modules/menu/menu.js
+++ src/blocks/modules/menu/menu.js
@@ -1,56 +1,61 @@
-const menu = document.querySelector('.menu');
-const submenu_b = document.querySelector('.menu__submenu--big');
-const submenu_s = document.querySelector('.menu__submenu--small');
-const overlay = document.querySelector('.menu__overlay');
-const small_menu_list_id = ['1','3','5','7', '9', '11'];
+const menu = document.querySelector(".menu");
+const submenu_b = document.querySelector(".menu__submenu--big");
+const submenu_s = document.querySelector(".menu__submenu--small");
+const overlay = document.querySelector(".menu__overlay");
+const body = document.querySelector("body");
+const small_menu_list_id = ["1", "3", "5", "7", "9", "11"];
 let current_menu_item = 0;
 
+menu.addEventListener("click", (e) => {
+  if (!e.target.closest(".menu__item")) return;
 
-menu.addEventListener('click', (e) => {
-    if(!e.target.closest('.menu__item')) return;
+  menu
+    .querySelectorAll(".menu__item")
+    .forEach((item, i) => (item.dataset.id = i + 1));
 
-    menu.querySelectorAll('.menu__item').forEach((item, i) => item.dataset.id = i + 1);
+  if (small_menu_list_id.includes(e.target.closest(".menu__item").dataset.id)) {
+    toggleSmallMenu(e.target);
+    return;
+  }
 
-    if(small_menu_list_id.includes(e.target.closest('.menu__item').dataset.id)) {
-        toggleSmallMenu(e.target);
-        return;
-    }
+  toggleBigMenu(e.target);
+});
 
-    toggleBigMenu(e.target);
+overlay.addEventListener("click", () => {
+  submenu_b.classList.remove("active");
+  submenu_s.classList.remove("active");
+  overlay.classList.remove("active");
+  body.classList.remove("unscrollable");
+  current_menu_item = 0;
+});
 
-})
-
-overlay.addEventListener('click', () => {
-    submenu_b.classList.remove('active');
-    submenu_s.classList.remove('active');
-    overlay.classList.remove('active');
+const toggleSmallMenu = (submenu) => {
+  if (submenu.closest(".menu__item").dataset.id == current_menu_item) {
+    submenu_s.classList.remove("active");
+    overlay.classList.remove("active");
     current_menu_item = 0;
-})
+    return;
+  }
 
-const toggleSmallMenu = submenu => {
-    if(submenu.closest('.menu__item').dataset.id == current_menu_item) {
-        submenu_s.classList.remove('active');
-        overlay.classList.remove('active');
-        current_menu_item = 0;
-        return;
-    }
+  submenu_b.classList.remove("active");
+  submenu_s.classList.add("active");
+  overlay.classList.add("active");
+  body.classList.add("unscrollable");
+  current_menu_item = submenu.closest(".menu__item").dataset.id;
+};
 
-    submenu_b.classList.remove('active');
-    submenu_s.classList.add('active');
-    overlay.classList.add('active');
-    current_menu_item = submenu.closest('.menu__item').dataset.id;
-}
+const toggleBigMenu = (submenu) => {
+  if (submenu.closest(".menu__item").dataset.id == current_menu_item) {
+    submenu_b.classList.remove("active");
+    overlay.classList.remove("active");
+    body.classList.remove("unscrollable");
+    current_menu_item = 0;
+    return;
+  }
 
-const toggleBigMenu = submenu => {
-    if(submenu.closest('.menu__item').dataset.id == current_menu_item) {
-        submenu_b.classList.remove('active');
-        overlay.classList.remove('active');
-        current_menu_item = 0;
-        return;
-    }
-
-    submenu_s.classList.remove('active');
-    submenu_b.classList.add('active');
-    overlay.classList.add('active');
-    current_menu_item = submenu.closest('.menu__item').dataset.id;
-}
+  submenu_s.classList.remove("active");
+  submenu_b.classList.add("active");
+  overlay.classList.add("active");
+  body.classList.add("unscrollable");
+  current_menu_item = submenu.closest(".menu__item").dataset.id;
+};
--- src/blocks/modules/promo/promo.js
+++ src/blocks/modules/promo/promo.js
@@ -3,18 +3,15 @@ import Swiper, { Autoplay, Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination, Autoplay]);
 
 const swiper = new Swiper(".swiper-container.promo__slider", {
-  // Optional parameters
-  // direction: 'vertical',
   loop: true,
   slidesPerView: 1,
   spaceBetween: 30,
   centeredSlides: true,
-  // autoplay: {
-  //   delay: 3500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
