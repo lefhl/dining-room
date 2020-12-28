994450230ea4d213a0ee71aea91baa8235729102
e.gizbrekht <gizbreht.e@yandex.ru>
2020-12-28 21:06:22 +0300

    Поисковая строка во многом улучшена и избавлена от багов. Осталось совсем немного доделать

--- src/blocks/modules/search/search.js
+++ src/blocks/modules/search/search.js
@@ -1,96 +1,109 @@
-// Поисковая строка
-const search = document.querySelector(".search");
-const search_filter = document.querySelector(".search__filter");
-const search_menu = search_filter.querySelector(".search__categories");
-const search_line = document.querySelector(".search__search-line");
-const results_wrap = document.querySelector(".search__results-wrapper");
-const results = document.querySelector(".search__results");
-const result = results.querySelectorAll(".search__result");
+document.addEventListener('DOMContentLoaded', () => {
+    // Поисковая строка
+  const search = document.querySelector(".search");
+  const search_container = search.querySelector(".search__search-container");
+  const search_filter = document.querySelector(".search__filter");
+  const search_menu = search_filter.querySelector(".search__categories");
+  const search_line = document.querySelector(".search__search-line");;
+  const search_btn = search.querySelector(".search__btn");
+  const results = document.querySelector(".search__results");
+  const overlay = document.querySelector('.overlay');
 
-// Дистрибьюторы и корзина
-const distr = document.querySelector(".search__distributors");
-const cart_value = document.querySelector(".search__cart-cost");
-const cart_text_arr = cart_value.textContent.split("");
-const cart = document.querySelector(".search__cart");
-const cart_icon = cart.querySelector(".search__icon");
+  // Дистрибьюторы и корзина
+  const distr = document.querySelector(".search__distributors");
+  const cart_value = document.querySelector(".search__cart-cost");
+  const cart = document.querySelector(".search__cart");
+  const cart_icon = cart.querySelector(".search__icon");
 
-// Телефон
-const search_phone_block = document.querySelector(".search__phone");
-
-results.style.left =
-  search_line.getBoundingClientRect().left + pageXOffset + "px";
-results.style.width = search_line.getBoundingClientRect().width + "px";
-
-cart_value.textContent =
-  cart_text_arr.reduce((res, letter, i) => {
-    if ((i + 1) % 3 === 0 && cart_text_arr.length !== i + 1) {
-      letter += " ";
-    }
-    res += letter;
-    return res;
-  }) + " руб.";
-
-search_filter.addEventListener("click", (e) => {
-  if (e.target.closest(".search__categories")) return;
-
-  search_menu.classList.toggle("active");
-});
-
-window.addEventListener("click", (e) => {
-  if (e.target.closest(".search__filter")) return;
-
-  search_menu.classList.remove("active");
-});
-
-search_line.addEventListener("input", () => {
-  if (search_line.value == "") {
-    results_wrap.classList.remove("active");
-    return;
+  
+  cart_value.textContent = cart_value.textContent.split('')
+    .reduce((res, lr, i, arr) => (i + 1) % 3 === 0 && arr.length !== i + 1 ? res += lr + ' ' : res += lr) + " руб.";
+    
+  if (window.matchMedia("(max-width: 1050px)").matches) {
+    search_btn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      search_container.classList.toggle("active");
+      results.style.width = search_line.getBoundingClientRect().width + "px";
+      results.style.left = search_line.getBoundingClientRect().x + "px";
+    });
+  } else {
+      results.style.left = 1 + search_filter.getBoundingClientRect().width + "px";
+      results.style.width = search_line.getBoundingClientRect().width + "px";
   }
-  results_wrap.classList.add("active");
-});
 
-window.addEventListener("click", (e) => {
-  if (
-    e.target.classList.contains("search__search-line") ||
-    e.target.closest(".search__results")
-  )
-    return;
+  window.addEventListener(`resize`, () => {
+    setTimeout(() => {
+      results.style.width = search_line.getBoundingClientRect().width + "px";
+      results.style.left = search_line.getBoundingClientRect().x + "px";
+    }, 300)
+  });
 
-  results_wrap.classList.remove("active");
-});
+  search_filter.addEventListener("click", (e) => {
+    if (e.target.closest(".search__categories")) return;
 
-search_menu.addEventListener("click", (e) => {
-  if (!e.target.closest(".search__categories-item")) return;
+    search_menu.classList.toggle("active");
+  });
 
-  let selected_category = search_filter.querySelector(
-    ".search__selected-category"
-  );
+  window.addEventListener("click", (e) => {
+    if (e.target.closest(".search__filter")) return;
 
-  search_menu
-    .querySelectorAll(".search__categories-item")
-    .forEach((child) => child.classList.remove("active"));
+    search_menu.classList.remove("active");
+  });
 
-  let list_item = e.target.closest(".search__categories-item");
-  list_item.classList.add("active");
+  search_line.addEventListener("input", () => {
+    if (search_line.value == "") {
+      results.classList.remove("active");
+      overlay.classList.remove('active', 'active--search');
+      return;
+    }
+    results.classList.add("active");
+    
+    if(window.matchMedia("(max-width: 1050px)").matches) return;
+    overlay.classList.add('active', 'active--search');
+  });
 
-  selected_category.textContent = e.target.textContent;
-  search_menu.classList.remove("active");
-});
+  window.addEventListener("click", (e) => {
+    if (e.target.classList.contains("search__search-line") || e.target.closest(".search__results")) return;
 
-distr.addEventListener("mouseenter", () => {
-  let icon = distr.querySelector(".search__icon");
-  icon.src = "../../../img/distr_hover.png";
-});
+    // results_wrap.classList.remove("active");
+  });
 
-distr.addEventListener("mouseleave", () => {
-  let icon = distr.querySelector(".search__icon");
-  icon.src = "../../../img/distr.png";
-});
+  search_menu.addEventListener("click", (e) => {
+    if (!e.target.closest(".search__categories-item")) return;
 
-cart.addEventListener("mouseenter", () => {
-  cart_icon.src = "../../../img/cart-hover.png";
-});
-cart.addEventListener("mouseleave", () => {
-  cart_icon.src = "../../../img/cart.png";
-});
+    let selected_category = search_filter.querySelector(".search__selected-category");
+
+    search_menu.querySelectorAll(".search__categories-item").forEach((child) => child.classList.remove("active"));
+
+    let list_item = e.target.closest(".search__categories-item");
+    list_item.classList.add("active");
+
+    selected_category.textContent = e.target.textContent;
+    search_menu.classList.remove("active");
+  });
+
+  distr.addEventListener("mouseenter", () => {
+    let icon = distr.querySelector(".search__icon");
+    icon.src = "../../../img/distr_hover.png";
+  });
+
+  distr.addEventListener("mouseleave", () => {
+    let icon = distr.querySelector(".search__icon");
+    icon.src = "../../../img/distr.png";
+  });
+
+  cart.addEventListener("mouseenter", () => {
+    cart_icon.src = "../../../img/cart-hover.png";
+  });
+  cart.addEventListener("mouseleave", () => {
+    cart_icon.src = "../../../img/cart.png";
+  });
+
+  search_container.addEventListener('click', (e) => {
+    if(e.target.classList.contains('search__search-container')) {
+      const wrapper = e.target;
+
+      wrapper.classList.remove('active');
+    }
+  })
+})
--- src/blocks/modules/seo/seo.js
+++ src/blocks/modules/seo/seo.js
@@ -2,6 +2,8 @@ const btn = document.querySelector(".seo__btn");
 const content_part = document.querySelector(".seo__content-part:last-child");
 
 btn.addEventListener("click", () => {
+  if (window.innerWidth > 760) return;
+
   content_part.classList.toggle("active");
   if (content_part.classList.contains("active")) {
     btn.textContent = "-";
